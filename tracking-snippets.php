3320c07802cc152a3be19bb52b007b1f6a7e2454
Thomas Peduto <tpeduto@stargroup1.com>
2014-02-20 17:09:50 -0500

    Initial commit

---
+++ tracking-snippets.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Tracking Snippets
+Plugin URL: http://friendsofpubliced.org
+Description: This plugin allows you to drop various analytics tracking snippets into individual pages.
+Author: Thomas Peduto
+Author URI: http://github.com/pedootz
+Version 0.1
+*/
